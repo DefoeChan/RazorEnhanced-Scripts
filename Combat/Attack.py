2976227fba7b5ff30b70e4643733c5f2d336b46f
Defoe Chan <defoe@defoechan.com>
2022-04-10 00:18:35 +0100

    Update Attack.py

--- Combat/Attack.py
+++ Combat/Attack.py
@@ -5,32 +5,32 @@ mobs = Target.GetTargetFromList( 'mobs' )
 greys = Target.GetTargetFromList( 'greys' )
 
 if pks != None:
+    Player.HeadMessage( colors[ 'red' ], 'PKs nearby!' )
     if Target.HasTarget():
         Target.TargetExecute( pks )
     else:
         Player.Attack( pks )
         
         Target.SetLast( pks )
-        Player.HeadMessage( colors[ 'red' ], 'PKs nearby!' )
         
 elif mobs != None:
+    Player.HeadMessage( colors[ 'red' ], 'Mobs nearby!' )
     if Target.HasTarget():
         Target.TargetExecute( mobs )
     else:
         Player.Attack( mobs )
         
     Target.SetLast( mobs )
-    Player.HeadMessage( colors[ 'red' ], 'Mobs nearby!' )
     while not Mobiles.FindBySerial(mobs) == None:
         Misc.Pause(500)
 elif greys != None:
+    Player.HeadMessage( colors[ 'red' ], 'Greys' + str(greys) + 'nearby!' )
     if Target.HasTarget():
         Target.TargetExecute( greys )
     else:
         Player.Attack( greys )
         
     Target.SetLast( greys )
-    Player.HeadMessage( colors[ 'red' ], 'Greys' + str(greys) + 'nearby!' )
     while not Mobiles.FindBySerial(greys) == None:
         Misc.Pause(500)
 else:
