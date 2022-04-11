eb6652b8765110b20e4bd53c2cffded787d72a34
Defoe Chan <defoe@defoechan.com>
2022-04-11 22:12:13 +0100

    Update Attack.py

--- Combat/Attack.py
+++ Combat/Attack.py
@@ -1,39 +1,26 @@
 from Scripts.Glossary.colors import colors
 
+def AttackMelee(enemy):
+    Player.HeadMessage( colors[ 'red' ], 'Greys ' + str(enemy.Serial) + ' nearby!' )
+    if Target.HasTarget():
+        Target.TargetExecute( emeny.Serial )
+    else:
+        Player.Attack( enemy )
+        Target.SetLast( enemy )
+        while not Mobiles.FindBySerial(enemy.Serial) == None:
+            Misc.Pause(500)
+    Player.HeadMessage( colors[ 'green' ], 'The enemy is dead.' )
+
 pks = Target.GetTargetFromList( 'pks' )
 mobs = Target.GetTargetFromList( 'mobs' )
 greys = Target.GetTargetFromList( 'greys' )
 
 if pks != None:
-    Player.HeadMessage( colors[ 'red' ], 'PKs nearby!' )
-    if Target.HasTarget():
-        Target.TargetExecute( pks )
-    else:
-        Player.Attack( pks )
-        
-        Target.SetLast( pks )
-        
+    AttackMelee(pks)
 elif mobs != None:
-    Player.HeadMessage( colors[ 'red' ], 'Mobs nearby!' )
-    if Target.HasTarget():
-        Target.TargetExecute( mobs )
-    else:
-        Player.Attack( mobs )
-        
-    Target.SetLast( mobs )
-    while not Mobiles.FindBySerial(mobs) == None:
-        Misc.Pause(500)
+    AttackMelee(mobs)
 elif greys != None:
-    Player.HeadMessage( colors[ 'red' ], 'Greys ' + str(greys.Serial) + ' nearby!' )
-    if Target.HasTarget():
-        Target.TargetExecute( greys.Serial )
-    else:
-        Player.Attack( greys )
-        
-    Target.SetLast( greys )
-    while not Mobiles.FindBySerial(greys.Serial) == None:
-        Misc.Pause(500)
-    Player.HeadMessage( colors[ 'green' ], 'The enemy is dead.' )
+    AttackMelee(greys)
 else:
     Misc.Pause( 100 )
 
