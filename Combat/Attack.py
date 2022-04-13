b0df16d4246c1336e9a71ae8cf70d9e3850a9f5f
Defoe Chan <defoe@defoechan.com>
2022-04-13 21:57:22 +0100

    Update Attack.py

--- Combat/Attack.py
+++ Combat/Attack.py
@@ -1,6 +1,6 @@
 from Scripts.Glossary.colors import colors
 
-def AttackMelee(enemy):
+def SearchDestroy(enemy):
     Player.HeadMessage( colors[ 'red' ], 'Greys ' + str(enemy.Name) + ' nearby!' )
     if Target.HasTarget():
         Target.TargetExecute( emeny.Serial )
@@ -8,87 +8,68 @@ def AttackMelee(enemy):
         Player.Attack( enemy )
         Target.SetLast( enemy )
         while not Mobiles.FindBySerial(enemy.Serial) == None:
-            mobilePosition = enemy.Position
-            Player.PathFindTo( mobilePosition.X, mobilePosition.Y , mobilePosition.Z )
+            Chase(enemy)
+            Player.HeadMessage( colors[ 'green' ], 'Chasing' )
+#            Player.PathFindTo( enemyPosition.X, enemyPosition.Y , enemyPosition.Z )
 #            while PathFinding = True
-            Misc.Pause(3000)
+            Misc.Pause(1000)
     Player.HeadMessage( colors[ 'green' ], 'The enemy is dead.' )
     
     
-def FollowMobile( mobile, maxDistanceToMobile = 1.5, startPlayerStuckTimer = False ):
-    '''
-    Uses the X and Y coordinates of the animal and player to follow the animal around the map
-    Returns True if player is not stuck, False if player is stuck
-    '''
+def Chase(enemy):
+    cantGetThereMax = 50
+    cantGetThere = 0
+    PathFind = False
+    
+    LocParty = enemy.Position
+    LocSelf = Player.Position
+    LocDiffX = LocParty.X - LocSelf.X
+    LocDiffY = LocParty.Y - LocSelf.Y
 
-    if not Timer.Check( 'catchUpToAnimalTimer' ):
-        return False
+    if LocDiffX >= 2 or LocDiffX <= -2 or LocDiffY >= 2 or LocDiffY <= -2: PathFind = True
 
-    mobilePosition = mobile.Position
-    playerPosition = Player.Position
-    directions = []
-    if mobilePosition.X > playerPosition.X and mobilePosition.Y > playerPosition.Y:
-        directions.append( 'Down' )
-    if mobilePosition.X < playerPosition.X and mobilePosition.Y > playerPosition.Y:
-        directions.append( 'Left' )
-    if mobilePosition.X > playerPosition.X and mobilePosition.Y < playerPosition.Y:
-        directions.append( 'Right' )
-    if mobilePosition.X < playerPosition.X and mobilePosition.Y < playerPosition.Y:
-        directions.append( 'Up' )
-    if mobilePosition.X > playerPosition.X and mobilePosition.Y == playerPosition.Y:
-        directions.append( 'East' )
-    if mobilePosition.X < playerPosition.X and mobilePosition.Y == playerPosition.Y:
-        directions.append( 'West' )
-    if mobilePosition.X == playerPosition.X and mobilePosition.Y > playerPosition.Y:
-        directions.append( 'South' )
-    if mobilePosition.X == playerPosition.X and mobilePosition.Y < playerPosition.Y:
-        directions.append( 'North' )
+    while PathFind == True:
+        LocParty = enemy.Position # Your main characters position
+        LocSelf = Player.Position # This characters position
+        LocDiffX = LocParty.X - LocSelf.X #The difference of x coordinates
+        LocDiffY = LocParty.Y - LocSelf.Y #The difference of y coordinates
+        cantGetThere += 1
+        if cantGetThere >= cantGetThereMax: break
+        #Find the most direct path to make the coordinates meet    
+        if LocDiffX < -1:
+            if LocDiffY < -1:
+                Player.Run('Up', False)
+            elif LocDiffY == -1 or LocDiffY == 0:
+                Player.Run('West', False)
+            else:
+                Player.Run('Left', False)
+        elif LocDiffX > 1:
+            if LocDiffY > 1:
+                Player.Run('Down', False)
+            elif LocDiffY == 1 or LocDiffY == 0:
+                Player.Run('East', False)
+            else:
+                Player.Run('Right', False)
+        elif LocDiffY < -1:
+            Player.Run('North', False)
+        elif LocDiffY > 1:
+            Player.Run('South', False)
+        else:
+            PathFind = False
+            cantGetThere = 0
+        Misc.Pause(20)
 
-    if startPlayerStuckTimer:
-        Timer.Create( 'playerStuckTimer', playerStuckTimerMilliseconds )
-
-    playerPosition = Player.Position
-    for direction in directions:
-        PlayerWalk( direction )
-
-    newPlayerPosition = Player.Position
-    if playerPosition == newPlayerPosition and not Timer.Check( 'playerStuckTimer' ):
-        # Player has been stuck in the same position for a while, try to find them a way out of the stuck position
-        if Player.Direction == 'Up':
-            for i in range ( 5 ):
-                Player.Walk( 'Down' )
-        elif Player.Direction == 'Down':
-            for i in range( 5 ):
-                Player.Walk( 'Up' )
-        elif Player.Direction == 'Right':
-            for i in range( 5 ):
-                Player.Walk( 'Left' )
-        elif Player.Direction == 'Left':
-            for i in range( 5 ):
-                Player.Walk( 'Right' )
-        Timer.Create( 'playerStuckTimer', playerStuckTimerMilliseconds )
-    elif playerPosition != newPlayerPosition:
-        Timer.Create( 'playerStuckTimer', playerStuckTimerMilliseconds )
-
-    if Player.DistanceTo( mobile ) > maxDistanceToMobile:
-        # This pause may need further tuning
-        # Dont want to create a ton of infinite calls if the player is stuck, but also dont want to not be able to catch up to animals
-        Misc.Pause( 100 )
-        FollowMobile( mobile, maxDistanceToMobile )
-        #pathfindToMobile( mobile )
-
-    return True
 
 pks = Target.GetTargetFromList( 'pks' )
 mobs = Target.GetTargetFromList( 'mobs' )
 greys = Target.GetTargetFromList( 'greys' )
 
 if pks != None:
-    AttackMelee(pks)
+    SearchDestroy(pks)
 elif mobs != None:
-    AttackMelee(mobs)
+    SearchDestroy(mobs)
 elif greys != None:
-    AttackMelee(greys)
+    SearchDestroy(greys)
 else:
     Misc.Pause( 100 )
 
