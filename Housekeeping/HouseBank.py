72c4c741608d60a258b2fb61a1538bc1a9af4868
Defoe Chan <defoe@defoechan.com>
2022-06-03 05:02:23 +0100

    House

--- Housekeeping/HouseBank.py
+++ Housekeeping/HouseBank.py
@@ -1,3 +1,5 @@
+import sys
+
 def HouseBank():
     global status
     if Player.DistanceTo(Items.FindBySerial(0x41BDCCC7)) <= 2:
@@ -6,6 +8,8 @@ def HouseBank():
         Organizer.RunOnce('reagents', Player.Backpack.Serial, 0x40A7B9F2, 500)
         Misc.Pause(500)
         Organizer.RunOnce('loot', Player.Backpack.Serial, 0x433CECD9, 500)
+        Misc.Pause(500)
+        Organizer.RunOnce('unidentified', Player.Backpack.Serial, 0x433CECD9, 500)
         Misc.Pause(500)
         if Player.GetRealSkillValue('Magery') >= 30:
             Restock.RunOnce('reagents', 0x40A7B9F2, Player.Backpack.Serial, 500)
@@ -18,6 +22,7 @@ def HouseBank():
             Misc.Pause(500)
     while Player.Hits < Player.HitsMax or Player.Mana < Player.ManaMax or Player.Stam < Player.StamMax: Misc.Pause(1000)
     status = 'Ready'
+    sys.exit()
     
     
 HouseBank()
---
+++ Testing/Testing 4.py
@@ -0,0 +1,4 @@
+
+Items.UseItem(0x4006386E)
+Target.WaitForTarget(10000, False)
+Target.Target(0x41745579)
