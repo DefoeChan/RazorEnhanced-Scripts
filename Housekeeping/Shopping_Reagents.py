cd939b907ff892607a5ce1f10d60ca1e4cbf8f93
Defoe Chan <defoe@defoechan.com>
2022-06-11 19:03:11 +0100

    Update Shopping_Reagents.py

--- Housekeeping/Shopping_Reagents.py
+++ Housekeeping/Shopping_Reagents.py
@@ -1,6 +1,6 @@
 # Vesper 1
-Misc.WaitForContext(0x0008A454, 10000)
 Misc.ContextReply(0x0008A454, 1)
-Misc.WaitForContext(0x0008A455, 10000)
 Misc.ContextReply(0x0008A455, 1)
 #Vesper 2
+Misc.ContextReply(0x0004924A, 1)
+Misc.ContextReply(0x00048D7C, 1)
