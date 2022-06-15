ad33f2e3429bb5ea70af3bce5e07e1fe3fa49856
Defoe Chan <defoe@defoechan.com>
2022-06-16 00:02:45 +0100

    Update Testing 4.py

--- Testing/Testing 4.py
+++ Testing/Testing 4.py
@@ -1,16 +1,39 @@
 Journal.Clear()
-pack = Items.FindBySerial(Target.PromptTarget())
-bagok = Items.FindBySerial(Target.PromptTarget())
-for itemcontenuti in pack.Contains:
+chestLoot = Items.FindBySerial(0x4360F2E5)
+chestVanquishing = Items.FindBySerial(0x42A851FD)
+chestRepond = Items.FindBySerial(0x42912AF1)
+trashcan = Items.FindBySerial(0x413220D0)
+
+for iteminchest in chestLoot.Contains:
     Player.UseSkill("Item ID")
     Target.WaitForTarget(5000)
-    Target.TargetExecute(itemcontenuti.Serial)
+    Target.TargetExecute(iteminchest.Serial)
     Misc.Pause(1500)
-    if Journal.Search("Vanquishing") == 1:
-        Misc.SendMessage("--> OK");
-        Items.Move(itemcontenuti , bagok, 0)
+    if Journal.Search("Repond") == 1 or \
+       Journal.Search("Silver") == 1:
+        Misc.SendMessage("--> Slayer");
+        Items.Move(iteminchest , chestRepond, 0)
         Misc.Pause(1000)
-    else:
-        Misc.SendMessage("--> No");
+    elif 
+        Journal.Search("Vanquishing") == 1:
+        Misc.SendMessage("--> Vanquishing");
+        Items.Move(iteminchest , chestVanquishing, 0)
+        Misc.Pause(1000)
+    elif Journal.Search("orcish pickaxe") == 1 or \
+         Journal.Search("Orcish Pickaxe") == 1:
+        Misc.SendMessage("Trash");
+        Items.Move(iteminchest , trashcan, 0)
+        Misc.Pause(1000)
+    elif Journal.Search("Ruin") == 1 or \
+         Journal.Search("Might") == 1 or \
+         Journal.Search("Force") == 1 or \
+         Journal.Search("Power") == 1 or \
+         Journal.Search("Defense") == 1 or \
+         Journal.Search("Guarding") == 1 or\
+         Journal.Search("Hardening") == 1:
+        Misc.SendMessage("--> Ruin / Might / Force");
+        Items.Move(iteminchest , Player.Backpack, 0)
+        Misc.Pause(1000)
     Journal.Clear()
+    
 Misc.SendMessage("DONE");
