6855b760f1dc21f177519e1f2b18f5740cb206b0
Defoe Chan <defoe@defoechan.com>
2022-06-15 15:41:31 +0100

    Update Testing 4.py

--- Testing/Testing 4.py
+++ Testing/Testing 4.py
@@ -1,4 +1,16 @@
-
-Items.UseItem(0x4006386E)
-Target.WaitForTarget(10000, False)
-Target.Target(0x41745579)
+Journal.Clear()
+pack = Items.FindBySerial(Target.PromptTarget())
+bagok = Items.FindBySerial(Target.PromptTarget())
+for itemcontenuti in pack.Contains:
+    Player.UseSkill("Item ID")
+    Target.WaitForTarget(5000)
+    Target.TargetExecute(itemcontenuti.Serial)
+    Misc.Pause(1500)
+    if Journal.Search("Vanquishing") == 1:
+        Misc.SendMessage("--> OK");
+        Items.Move(itemcontenuti , bagok, 0)
+        Misc.Pause(1000)
+    else:
+        Misc.SendMessage("--> No");
+    Journal.Clear()
+Misc.SendMessage("DONE");
